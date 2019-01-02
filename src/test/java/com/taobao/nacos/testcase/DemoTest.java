0c1fa8f74ae0fdd8b914ea6ef05b235fa8a0aacd
gaoyunpeng <zhendong590@gmail.com>
2019-01-02 17:50:31 +0800

    update testcase

---
+++ src/test/java/com/taobao/nacos/testcase/DemoTest.java
@@ -0,0 +1,12 @@
+package com.taobao.nacos.testcase;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DemoTest {
+
+    @Test
+    public void demo(){
+        Assert.assertTrue(true);
+    }
+}
